ec770b4b46443e7d239a551b168703daae7572ff
Vatsal <neelgupta701@gmail.com>
2024-01-31 16:46:49 +0530

    admin herder

--- resources/views/admin/includes/header.blade.php
+++ resources/views/admin/includes/header.blade.php
@@ -24,14 +24,14 @@
             <li class="dropdown dropdown-animated scale-left">
                 <div class="pointer" data-toggle="dropdown">
                     <div class="avatar avatar-image  m-h-10 m-r-15">
-                        <img src="@if(!empty($siteSetting) &&  !empty($siteSetting->logo) && file_exists(base_path(" uploads/setting/".$siteSetting->logo))){{asset("uploads/setting/".$siteSetting->logo)}}@else{{ asset('assets/images/logo/logo.png') }}@endif" alt="">
+                        <img src="@if(!empty($siteSetting) &&  !empty($siteSetting->logo) && file_exists(base_path("uploads/setting/".$siteSetting->logo))){{asset("uploads/setting/".$siteSetting->logo)}}@else{{ asset('assets/images/logo/logo.png') }}@endif" alt="">
                     </div>
                 </div>
                 <div class="p-b-15 p-t-20 dropdown-menu pop-profile">
                     <div class="p-h-20 p-b-15 m-b-10 border-bottom">
                         <div class="d-flex m-r-50">
                             <div class="avatar avatar-lg avatar-image">
-                                <img src="@if(!empty($siteSetting) &&  !empty($siteSetting->logo) && file_exists(base_path(" uploads/setting/".$siteSetting->logo))){{asset("uploads/setting/".$siteSetting->logo)}}@else{{ asset('assets/images/logo/logo.png') }}@endif" alt="">
+                                <img src="@if(!empty($siteSetting) &&  !empty($siteSetting->logo) && file_exists(base_path("uploads/setting/".$siteSetting->logo))){{asset("uploads/setting/".$siteSetting->logo)}}@else{{ asset('assets/images/logo/logo.png') }}@endif" alt="">
                             </div>
                             <div class="m-l-10">
                                 <p class="m-b-0 text-dark font-weight-semibold">{{ Auth::user()->first_name . ' '. Auth::user()->last_name}}</p>
